0cccc9582590b29e22cd11156a372a3df0fb7134
Fooker <fookerbar@gmail.com>
2017-09-21 13:37:57 +0800

    Formatted reservation date selection and made turbolinks enable datepicker when going to another page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,15 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require rails-ujs
 //= require jquery3
 //= require popper
 //= require bootstrap-sprockets
+//= require bootstrap-datepicker
+//= require turbolinks
+//= require_tree .
+
+document.addEventListener("turbolinks:load", function() {
+	$('#startdatepicker').datepicker({orientation: 'bottom'});
+	$('#enddatepicker').datepicker({orientation: 'bottom'});
+});
