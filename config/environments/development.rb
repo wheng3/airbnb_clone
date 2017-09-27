d39e15379c3a6a279afac9e2a0f22df0f2e49800
Wee Jing Heng <wheng3@illinois.edu>
2017-09-27 11:35:05 +0800

    Enable email sending in production

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -58,16 +58,16 @@ Rails.application.configure do
   # Config to preview email before sending
   # config.action_mailer.delivery_method = :letter_opener
 
-  config.action_mailer.delivery_method = :smtp
-  # SMTP settings for gmail
-  config.action_mailer.smtp_settings = {
-  :address              => "smtp.gmail.com",
-  :port                 => 587,
-  :domain               =>'gmail.com',
-  :user_name            => ENV['GMAIL_USERNAME'],
-  :password             => ENV['GMAIL_PASSWORD'],
-  :authentication       => "plain",
-  :enable_starttls_auto => true
-  }
+  # config.action_mailer.delivery_method = :smtp
+  # # SMTP settings for gmail
+  # config.action_mailer.smtp_settings = {
+  # :address              => "smtp.gmail.com",
+  # :port                 => 587,
+  # :domain               =>'gmail.com',
+  # :user_name            => ENV['GMAIL_USERNAME'],
+  # :password             => ENV['GMAIL_PASSWORD'],
+  # :authentication       => "plain",
+  # :enable_starttls_auto => true
+  # }
   
 end
